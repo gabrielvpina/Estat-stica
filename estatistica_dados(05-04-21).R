4657d34c187835e138780e52e8f5b6418e139654
gabrielvpina <69880032+gabrielvpina@users.noreply.github.com>
2021-04-05 20:32:38 -0300

    Add files via upload
    Dados usados na aula em 05/04/2021, onde se empregou métodos da estatística descritiva através do programa RStudio.

---
+++ estatistica_dados(05-04-21).R
@@ -0,0 +1,16 @@
+getwd() # diretório de trabalho (pasta onde se importa o arquivo)
+
+dados <- read.csv(file = "Dados_estatistica(05-04-21).csv", header = TRUE, sep = "\t", dec = ",")
+#importando o arquivo (header = nome de coluna, sep = separação dos termos, dec = símbolo decimal)
+
+class(dados) # saber o tipo de variável
+
+str(dados) # vizualização de dados no console
+
+mean(dados$U.A.) # média de U.A.
+median(dados$U.A.) # mediana de U.A.
+sd(dados$U.A.) # desvio-padrão de U.A.
+max(dados$U.A.) # máxima de U.A.
+min(dados$U.A.) # mínima de U.A.
+summary(dados$U.A.) # várias informações dos dados
+quantile(dados$U.A.) # informa os quartis da sequência
