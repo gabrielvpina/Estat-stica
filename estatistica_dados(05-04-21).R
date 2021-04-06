abd95eeb12f3f39b99c621cd3125e30172c1cfc2
gabrielvpina <69880032+gabrielvpina@users.noreply.github.com>
2021-04-05 21:26:39 -0300

    Update estatistica_dados(05-04-21).R

--- estatistica_dados(05-04-21).R
+++ estatistica_dados(05-04-21).R
@@ -1,16 +1,23 @@
-getwd() # diretório de trabalho (pasta onde se importa o arquivo)
+getwd() # diretÃ³rio de trabalho (pasta onde se importa o arquivo)
 
 dados <- read.csv(file = "Dados_estatistica(05-04-21).csv", header = TRUE, sep = "\t", dec = ",")
-#importando o arquivo (header = nome de coluna, sep = separação dos termos, dec = símbolo decimal)
+#importando o arquivo (header = nome de coluna, sep = separaÃ§Ã£o dos termos, dec = sÃ­mbolo decimal)
 
-class(dados) # saber o tipo de variável
+class(dados) # saber o tipo de variÃ¡vel
 
-str(dados) # vizualização de dados no console
+str(dados) # vizualizaÃ§Ã£o de dados no console
 
-mean(dados$U.A.) # média de U.A.
+mean(dados$U.A.) # mÃ©dia de U.A.
 median(dados$U.A.) # mediana de U.A.
-sd(dados$U.A.) # desvio-padrão de U.A.
-max(dados$U.A.) # máxima de U.A.
-min(dados$U.A.) # mínima de U.A.
-summary(dados$U.A.) # várias informações dos dados
-quantile(dados$U.A.) # informa os quartis da sequência
+sd(dados$U.A.) # desvio-padrÃ£o de U.A.
+max(dados$U.A.) # mÃ¡xima de U.A.
+min(dados$U.A.) # mÃ­nima de U.A.
+summary(dados$U.A.) # vÃ¡rias informaÃ§Ãµes dos dados
+quantile(dados$U.A.) # informa os quartis da sequÃªncia
+range(dados$U.A.) # amplitude de U.A.
+
+Q1 <- quantile(dados$U.A., probs = 0.25)
+Q3 <- quantile(dados$U.A., probs = 0.75)
+
+IQR = Q3 - Q1 # amplitrude interquartil (IQR)
+print(IQR)
